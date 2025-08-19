b3ba5b0ed77202d7b556c4ded0adb02fbb167625
bhavi967 <bhavyakarnam9@gmail.com>
2025-08-19 08:32:43 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Bhavya",
+    author_email="bhavyakarnam9@gmail.com",
+    packages=find_packages()
+)
